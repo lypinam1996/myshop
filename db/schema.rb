7cab6b2f25d9c247fc5b21e06dc2e64a6564e0f9
Maria <lypinam@rambler.ru>
2017-12-04 19:37:08 +0300

    Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203175507) do
+ActiveRecord::Schema.define(version: 20171204163128) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "Not specified", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171203175507) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
