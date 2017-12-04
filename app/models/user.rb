3f66edcb83b75c7c05caf77ba8c7baaeed39a22a
Maria <lypinam@rambler.ru>
2017-12-04 20:28:14 +0300

    Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
 	has_secure_password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: true
+
+	before_save { self.email = email.downcase }
 end
