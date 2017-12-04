6fccfc19f89c4590ca096d6dcdcb3e4e9810d6cb
Maria <lypinam@rambler.ru>
2017-12-04 21:59:04 +0300

    Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :conf_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def conf_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name, :balance ])
+  end
 end
