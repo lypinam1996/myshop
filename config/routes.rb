001f846b18c92783b01618897b49ecc7f4871bba
Maria <lypinam@rambler.ru>
2017-12-05 16:30:59 +0300

    Main page plus bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :authors
-  resources :books
+  resources :book
   resources :roles
   devise_for :users
   resources :users, except: [ :new, :create, :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to:'users#index'
-  root to: 'users#index'
+  root to: 'books#index'
 end
